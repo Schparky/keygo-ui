a6107931afb7ff73e7b45ca3feb3bc8a89238114
Schparky <3172830+Schparky@users.noreply.github.com>
2022-06-09 21:47:42 -0600

    changes I forgot about

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
 import { getUser, User } from './data/user'
 import Login from './Login'
+import logo from './logo.svg'
 import Profile from './Profile'
+
 import { useEffect, useState } from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 export default function App() {
 	const [user, setUser] = useState<User | null>(null)
@@ -10,12 +13,23 @@ export default function App() {
 	useEffect(() => updateUser(), [])
 
 	return (
-		<>
-			<div>
-				<Login />
-			</div>
-			<div>{user && <Profile user={user} />}</div>
-		</>
+		<div className="App">
+			<header className="App-header">
+				<nav
+					style={{
+						borderBottom: 'solid 1px',
+						paddingBottom: '1rem',
+					}}
+				>
+					<Link to="/invoices">Invoices</Link> | <Link to="/expenses">Expenses</Link>
+				</nav>
+				<Outlet />
+				<div className="m-3">
+					<Login />
+				</div>
+				<div>{user && <Profile user={user} />}</div>
+			</header>
+		</div>
 	)
 
 	function updateUser() {
